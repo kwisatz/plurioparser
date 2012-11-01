6d2bc6d1fbf3db46869dc1e6d800cbbc4f4549c0
David Raison <david@hackerspace.lu>
2012-11-01 16:13:58 +0100

    updated verify.sh

--- verify.sh
+++ verify.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-#URL="http://www.hackerspace.lu/plurio/"
 SCHEMA=plurio.xsd
 OUT=plurio.xml
-MAILTO="david@hackerspace.lu"
+MAILTO="david@raison.lu"
 LOGFILE=plurio.log
 TODAY=`date +%F`
 
 echo "Running plurio.net export-and-verify... "
 rm $OUT
 
-#`which wget` -q -O $OUT $URL
 `which php` pluriofeed.php
 
 [ -f $OUT ] && `which xmllint` --noout --schema $SCHEMA $OUT > ${LOGFILE} 2>&1
